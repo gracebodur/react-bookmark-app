db2a1d94d1c40debbe53d2b821155098b276de7f
Grace <mgracebodur@gmail.com>
2019-11-17 23:14:47 -0500

    Added fetch method

--- src/App.js
+++ src/App.js
@@ -21,55 +21,64 @@ const bookmarks = [
 
 class App extends React.Component {
 
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     bookmarks: [],
-  //     showAddForm: false
-  //   }
-  // }
+    //1. show either the bookmarkApp or 
+    //the addForm component started with 
+    //constructor this.state then pass it down to the Bookmark component
+  constructor(props) {
+    super(props)
+    this.state = {
+      bookmarks: [],
+      showAddForm: false
+    }
+  }
 
-  // componentDidMount() {
-  //   const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks'
-  //   const options = {
-  //     method: 'GET',
-  //     headers: {
-  //       "Authorization": "Bearer $2a$10$ZhdeJefcb.5sx/DCmO/n8u5sJLcARAdbHw9tfm1mevGRq3s1.5DpW",
-  //       "Content-Type": "application/json"
-  //     }
-  //   }
-
-  //   fetch(url, options)
-  //   .then(res => {
-  //     if(!res.ok) {
-  //       throw new Error('Something went wrong, please try again later')
-  //     }
-  //     return res
-  //   })
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     this.setState({
-  //       bookmarks: data,
-  //       error: null
-  //     })
-  //   })
-  //   .catch(err => {
-  //     this.setState({
-  //       error: err.message
-  //     })
-  //   })
-  // }
+  componentDidMount() {
+    const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks'
+    const options = {
+      method: 'GET',
+      headers: {
+        //add key after bearer
+        "Authorization": "Bearer $2a$10$w8uTcVCIeb8xJIrcP0XuWOTNMaBspXScWml3KCOU6jB9cjahUtT.a",
+        "Content-Type": "application/json"
+      }
+    }
+  fetch(url, options)
+    .then(res => {
+      if(!res.ok) {
+        throw new Error('something went wrong, try again later')
+      }
+      return res
+    })
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        bookmarks: data,
+        error: null
+      })
+    })
+    .catch(err => {
+      this.setState({
+        error: err.message
+      })
+    })
+  }
 
   render() {
-    // const page = this.state.showAddForm
-        // ? <AddBookmark />
-        // : <BookmarkApp bookmarks=
-        // {this.state.bookmarks} />
+
+    // 3. add a conditional display, 
+    //if the showAddForm is true then display the AddBookmark component, 
+    //otherwise display the BookmarkApp component.
+    const page = this.state.showAddForm
+        ? <AddBookmark />
+        : <BookmarkApp bookmarks=
+        {this.state.bookmarks} />
     return (
       <div className="App">
-        <AddBookmark />
-        <BookmarkApp bookmarks={bookmarks}/>
-        {/* {page} */}
+        {/* <AddBookmark /> 4. clear the componenst 
+        here pass the variable page to be displayed */}
+        {/* 2. added the state to bookmarkapp using this.state  */}
+        {/* <BookmarkApp bookmarks={this.state.bookmarksbookmarks}/>  */}
+        {page}
       </div>
     );
   }
